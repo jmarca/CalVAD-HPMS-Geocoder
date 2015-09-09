b49b570c3d644f843bee451c5fa73e6f4690767f
James E. Marca <james@activimetrics.com>
2015-09-09 11:55:15 -0700

    working.  Starting to move code from orig

--- lib/CalVAD/HPMS/Geocoder.pm
+++ lib/CalVAD/HPMS/Geocoder.pm
@@ -1,5 +1,28 @@
+# ABSTRACT: turns baubles into trinkets
 use strict;
 use warnings;
-package CalVAD::HPMS::Geocoder;
+use Moops;
 
+package CalVAD::HPMS;
+
+class Geocoder using Mosse : ro {
+    use Carp;
+    use Data::Dumper;
+    use English qw(-no_match_vars);
+    use version; our $VERSION = qv('0.1.0');
+
+    use CalVAD::HPMS::Geocoder::Schema;
+
+    use Geocoder::Cities;
+    use Geocoder::CensusAbbrev;
+
+    use DBI qw(:sql_types);
+    use DBD::Pg  qw(:pg_types);
+
+    has 'trigram' => (is => 'rw', isa => 'Bool', 'default'=>0);
+    has 'shwy' => (is => 'rw', isa => 'Bool', 'default'=>0);
+    has 'county' => (is => 'rw', isa => 'Bool', 'default'=>0);
+
+
+}
 1;
