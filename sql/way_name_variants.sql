a63aa31fd7e12000df00baf9104bd1dec0ba2cb6
James E. Marca <james@activimetrics.com>
2015-09-30 12:08:22 -0700

    way name view as materialized view

---
+++ sql/way_name_variants.sql
@@ -0,0 +1,43 @@
+-- create a table way name variants?  or make it a view?
+-- can views have indices?
+
+with
+    roadways as (
+        select *
+        from ways
+        where tags ? 'highway'
+    ),
+    tag_values as (
+        select id as id, svals(tags) as name, skeys(tags) as key
+        from roadways
+    ),
+    name_values as (
+        select id,name from tag_values
+        where key ~* 'name' and
+              key !~* '_direction' and
+              key !~* '_type'
+-- || quote_literal(name_pattern)
+-- || '
+    )
+select id, name into way_name_variants from name_values ;
+
+
+
+CREATE MATERIALIZED VIEW way_name_view AS
+with
+    roadways as (
+        select *
+        from ways
+        where tags ? 'highway'
+    ),
+    tag_values as (
+        select id as id, svals(tags) as name, skeys(tags) as key
+        from roadways
+    ),
+    name_values as (
+        select id,name from tag_values
+        where key ~* 'name' and
+              key !~* '_direction' and
+              key !~* '_type'
+    )
+select id, name from name_values ;
